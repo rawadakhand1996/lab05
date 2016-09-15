7edf65f98e40e36dff51a9d9ffa6fbfb0a9c009f
ABIE AKHAND <abieakhand@adelphi.internal>
2016-09-15 12:02:31 -0400

    edited lab05

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -14,44 +14,104 @@ public class PersonCreator
     private String sex;//--- male
     private int height;//--69
     private int weight;//--176
-    private int firstDigitSSN;//-- 123
-    private int secondDigitSSN;//--12
-    private int thirdDigitSSN;//--1234
+    private String SSN;//152-22-1564
     private String address;//-- 6063 Cedar Swamp Rd. Glen Head NY11545
     private int myDOB;// 05/03/1996
+    private String phoneNumber;
     
-    public PersonCreator(String myFirstName, String myLastName)
+    /* 
+     Description: Constructor
+     Input: first and last name
+     Output:no output
+     Example:-----
+     */
+public PersonCreator(String myFirstName, String myLastName)
     {
         firstName = myFirstName;// sets first name
         lastName = myLastName;// sets last name
+        phoneNumber ="000-000-0000";
+        SSN="000-00-0000";
 } 
 
-public void setAge(int myAge)
+/*
+ Description: sets age
+ Input: the person's age
+ Output: age
+ Example: 20
+ 
+ */public void setAge(int myAge)
 {
-    age=myAge;// sets age
+    age=myAge;
 }
 
-public void setSex(String gender)
+/*
+ Description: sets gender
+ Input: user's gender
+ Output: purpose is set to whatever the user identifies 
+ Example: male
+ */public void setSex(String gender)
 {
     sex=gender;// sets gender
 }
 
-public void setHeight(int inches)
+/*
+ Description: sets height
+ Input: user's height
+ Output: whatever height was given
+ Example: 69
+ */public void setHeight(int inches)
 {
     height=inches;// sets height
 }
 
-public void setWeight(int lbs)
+/*
+ Description: sets the weight of the user
+ Input: user's weight
+ Output: the weight
+ Example:174
+ */public void setWeight(int lbs)
 {
     weight=lbs;// sets weight
+}
+
+/*
+ Description: changes the phone number
+ Input: users input their numbers and area code in format
+ Output:Only outputs if error but purpose is to change to phone number from 000-00-000 to something the user wants
+ Example: 609-123-1155
+ */public void changeNumber(int areaCode, int numBegin, int numEnd)
+{
+    //if areaCode is bad, tell user to input something else,
+    if(areaCode < 100 ||areaCode > 999){
+        System.err.println("Print use a valid number,");
+    } else if (numBegin < 100 || numBegin> 999){
+        System.err.println("Please use a valid number.");
+    } else if (numEnd < 1000 || numEnd > 9999){
+        System.err.println("Please use a valid number.");
+    } else {// ifnot (areaCode < 100 || areaCode > 999) is not true,do
+        phoneNumber = areaCode + "-" + numBegin + "-" + numEnd;
+    }
+    // Once an if statement is true, the program with do it is told// and then skip to here.
 }
 
 
 
 
-public void setSSN(int firstThreeDigits, int secondThreeDigits, int thirdFourDigits)
+/*
+ * 
+ */public void setSSN(int firstThreeDigits, int secondThreeDigits, int thirdFourDigits)
 {
-    //sets the SSN in the correct format
+     //if areaCode is bad, tell user to input something else,
+    if(firstThreeDigits < 100 ||firstThreeDigits > 999){
+        System.err.println("Print use a valid number,");
+    } else if (secondThreeDigits < 10 || secondThreeDigits> 99){
+        System.err.println("Please use a valid number.");
+    } else if (thirdFourDigits< 1000 ||thirdFourDigits > 9999){
+        System.err.println("Please use a valid number.");
+    } else {// ifnot (areaCode < 100 || areaCode > 999) is not true,do
+        SSN = firstThreeDigits + "-" + secondThreeDigits + "-" + thirdFourDigits;
+    }
+    // Once an if statement is true, the program with do it is told// and then skip to here.
     
 }
 
@@ -74,7 +134,7 @@ public void PersonCreator()// prints out all the information
     System.out.println("Sex: " + sex);
     System.out.println("Height:" + height);
     System.out.println("Weight: " + weight + " lbs ");
-    System.out.println("SSN: " + firstDigitSSN + "-" + secondDigitSSN + "-" + thirdDigitSSN);
+    System.out.println("SSN: " + SSN);
     System.out.println("address: " +address);
 
 }

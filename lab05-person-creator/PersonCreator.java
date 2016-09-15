390fce42e9cab44481bd63bbbbd6eebac87c799d
ABIE AKHAND <abieakhand@adelphi.internal>
2016-09-15 11:20:27 -0400

    Continued and commented on everything in lab05

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -8,42 +8,42 @@
 public class PersonCreator
 {
     //
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String sex;
-    private int height;
-    private int weight;
-    private int firstDigitSSN;
-    private int secondDigitSSN;
-    private int thirdDigitSSN;
-    private String address;
+    private String firstName;// first name---Rawad
+    private String lastName;// last name--- Akhand
+    private int age;//---- 20
+    private String sex;//--- male
+    private int height;//--69
+    private int weight;//--176
+    private int firstDigitSSN;//-- 123
+    private int secondDigitSSN;//--12
+    private int thirdDigitSSN;//--1234
+    private String address;//-- 6063 Cedar Swamp Rd. Glen Head NY11545
     private int myDOB;// 05/03/1996
     
     public PersonCreator(String myFirstName, String myLastName)
     {
-        firstName = myFirstName;
-        lastName = myLastName;
+        firstName = myFirstName;// sets first name
+        lastName = myLastName;// sets last name
 } 
 
 public void setAge(int myAge)
 {
-    age=myAge;
+    age=myAge;// sets age
 }
 
 public void setSex(String gender)
 {
-    sex=gender;
+    sex=gender;// sets gender
 }
 
 public void setHeight(int inches)
 {
-    height=inches;
+    height=inches;// sets height
 }
 
 public void setWeight(int lbs)
 {
-    weight=lbs;
+    weight=lbs;// sets weight
 }
 
 
@@ -51,23 +51,23 @@ public void setWeight(int lbs)
 
 public void setSSN(int firstThreeDigits, int secondThreeDigits, int thirdFourDigits)
 {
-    
+    //sets the SSN in the correct format
     
 }
 
 public void setAddress(String street)
 {
-   address=street;
+   address=street; // sets street address according to the user's will
 }
 
 public void setDOB(int DOB)
 {
-    DOB=myDOB;
+    DOB=myDOB; // sets date of birth
 }
 
 
 
-public void PersonCreator()
+public void PersonCreator()// prints out all the information
 {
     System.out.println("Name: " + firstName +" " + lastName);
     System.out.println("Age: " + age);
